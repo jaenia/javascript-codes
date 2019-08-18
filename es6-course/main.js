b00574d7d574a7a42f114af2011a6e5a54bd44eb
Jaênia <jaeniaps@gmail.com>
2019-08-18 20:32:16 -0300

    Using babel and classes

--- es6-course/main.js
+++ es6-course/main.js
@@ -1,9 +1,38 @@
-class Teste {
-    metodo() {
+class List {
+  constructor() {
+    this.data = []
+  }
 
-    }
-
-    outro() {
-
-    }
+  add(data) {
+    this.data.push(data)
+    console.log(this.data)
+  }
 }
+
+class TodoList extends List {
+  constructor() {
+    super();
+    this.usuario = 'Jaênia'
+  }
+
+  mostraUsuario() {
+    console.log(this.usuario)
+  }
+}
+
+var MinhaLista = new TodoList()
+
+document.getElementById('novotodo').onclick = function() {
+  MinhaLista.add("Novo todo")
+}
+
+MinhaLista.mostraUsuario()
+
+// static class
+class Math {
+  static sum(a, b) {
+    return a + b
+  }
+}
+
+console.log(Math.sum(1, 2))
