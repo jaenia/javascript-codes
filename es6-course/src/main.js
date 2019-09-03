25b1aa3104e059ebbd42eab754c5a60de1c1ebb1
Jaênia <jaeniaps@gmail.com>
2019-09-03 10:31:50 -0300

    Add simple async/await

--- es6-course/src/main.js
+++ es6-course/src/main.js
@@ -2,10 +2,25 @@ const minhaPromise = () => new Promise((resolve, reject) =>
   setTimeout(() => { resolve('OK')}, 2000)
 )
 
-minhaPromise()
-  .then(response => {
-    console.log(response)
-  })
-  .catch(err => {
+// minhaPromise()
+//   .then(response => {
+//     console.log(response)
+//   })
+//   .catch(err => {
     
-  })
+//   })
+
+// async function executaPromise() {
+//   console.log(await minhaPromise())
+//   console.log(await minhaPromise())
+//   console.log(await minhaPromise())
+// }
+
+// using arrow function
+const executaPromise = async () => {
+  console.log(await minhaPromise())
+  console.log(await minhaPromise())
+  console.log(await minhaPromise())
+}
+
+executaPromise()
